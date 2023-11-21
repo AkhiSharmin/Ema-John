16a85082b3ef0f30a385a4725b88d2a7abdbd6c8
AkhiSharmin <akhisharmin100@gmail.com>
2023-11-22 00:54:44 +0600

    adding some style in product component

--- src/Components/Product/Product.jsx
+++ src/Components/Product/Product.jsx
@@ -1,10 +1,17 @@
 import "./Product.css";
 const Product = ({ product }) => {
-  const { img, name, seller, quantity, price } = product;
+  const { img, name, seller, ratings, price } = product;
 
   return (
     <div className="product">
       <img src={img} alt="" />
+      <div className="product-info">
+        <h6 className="product-name">{name}</h6>
+        <p>Price: {price}$</p>
+        <p>Manufacturer: {seller}</p>
+        <p>Rating: {ratings} Star</p>
+      </div>
+      <button className="btn-cart">Add to Cart</button>
     </div>
   );
 };
